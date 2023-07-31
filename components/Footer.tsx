a0e51cdeb1c42d96b42e008886e12d089d824157
Kang Jiaming <kangjm2005@hotmail.com>
2023-07-31 12:55:12 +0800

    feat: current visitors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { client } from '@umami/api-client'
 
-const Footer = () => {
+const getActiveUsers = async () => {
+  const id = process.env.NEXT_PUBLIC_WEBSITE_ID
+  const token = process.env.UMAMI_TOKEN
+  if (!id || !token) return null
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${token}` },
+    redirect: 'follow'
+  }
+  const response = await fetch("https://analytics.xyspg.moe/api/websites/2d67dea1-d9b6-46a6-bcde-02f612591fdf/active", requestOptions)
+  const data = await response.json()
+  const activeUsers = data[0].x
+  return activeUsers
+  
+}
+
+
+const Footer =async () => {
+  const activeUsers = await getActiveUsers()
+  
   return (
     <div className="text-xs text-neutral-600">
       <hr className="py-1 my-2" />
+      <p>
+        Current Visitors: {activeUsers}
+      </p>
       <p>
         The information in this website may be subject to change due to various
         circumstances. Dalton Academy reserves the right of final interpretation
