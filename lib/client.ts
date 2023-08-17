7c4e1f78bd6cfb6cf52fbfdf52567142f1d6f59c
Kang Jiaming <kangjm2005@hotmail.com>
2023-08-17 17:13:57 +0800

    use previous api version

--- lib/client.ts
+++ lib/client.ts
@@ -3,6 +3,6 @@ import { createClient } from "next-sanity";
 export default createClient({
   projectId: "fbgv2m2h",
   dataset: "production",
-  apiVersion: "2023-08-17",
+  apiVersion: "2023-07-20",
   useCdn: true,
 });
