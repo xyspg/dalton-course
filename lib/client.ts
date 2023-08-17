02e2069e6afc760fbe1969cca9c5ec81dc30b73a
Kang Jiaming <kangjm2005@hotmail.com>
2023-08-17 17:08:21 +0800

    edit sanity client

--- lib/client.ts
+++ lib/client.ts
@@ -4,5 +4,5 @@ export default createClient({
   projectId: "fbgv2m2h",
   dataset: "production",
   apiVersion: "2023-08-17",
-  useCdn: process.env.NODE_ENV === "development" ? false : true,
+  useCdn: true,
 });
