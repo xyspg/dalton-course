7e951b303665558b57a1ca47bc33fbe11737e70e
Kang Jiaming <kangjm2005@hotmail.com>
2023-08-20 10:53:31 +0800

    fix: window

--- lib/hooks/use-local-storage.ts
+++ lib/hooks/use-local-storage.ts
@@ -4,6 +4,7 @@ export const useLocalStorage = <T>(
     key: string,
     initialValue: T
 ): [T, (value: T) => void] => {
+  if (!window) return [initialValue, () => null];
   const storedValueFromLocalStorage = window.localStorage.getItem(key);
   const [storedValue, setStoredValue] = useState<T>(
       storedValueFromLocalStorage ? JSON.parse(storedValueFromLocalStorage) : initialValue
